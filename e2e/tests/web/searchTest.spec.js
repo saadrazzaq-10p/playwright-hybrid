f354323539dfa1fbdd36dc47e385260274eb2f7d
saadrazzaq-10p <saad.razzaq@10pearls.com>
2024-05-25 21:23:30 +0500

    updated web scenario

--- e2e/tests/web/searchTest.spec.js
+++ e2e/tests/web/searchTest.spec.js
@@ -3,13 +3,13 @@ const testData = require("../../../data/test-data.json");
 
 test.describe('Search Functionality', () => {
 
-    test.beforeEach(async ({ page, SearchPage }, testInfo) => {
-        await SearchPage.navigateToSearchPage()
+    test.beforeEach(async ({ page, WebSearchPage }, testInfo) => {
+        await WebSearchPage.navigateToSearchPage()
     })
 
-    test('Search for "jha900" and verify results', async ({SearchPage}) => {
-        await SearchPage.searchFor(testData.username);
-        const followersCount = await SearchPage.getFollowersCount();
+    test('Search for "jha900" and verify results', async ({WebSearchPage}) => {
+        await WebSearchPage.searchFor(testData.username);
+        const followersCount = await WebSearchPage.getFollowersCount();
         expect(followersCount).toBeVisible;
     });
 });
